24f6e5bc4ffcfe0d25a7d11ad1112f2cb3be7dbb
PedroBelchior <pfbe@netvisao.pt>
2015-03-29 22:45:33 +0100

    Error messages for Get and Set commands. Help command added. Get and Set commands now set fields Accessible before getting/setting.

--- src/ist/meic/pa/EvalShell.java
+++ src/ist/meic/pa/EvalShell.java
@@ -30,7 +30,7 @@ public class EvalShell {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		while(true) {
-			System.out.println("DebuggerCLI :> ");
+			System.out.print("DebuggerCLI :> ");
 			
 			String[] splitresult = null;
 
@@ -40,39 +40,87 @@ public class EvalShell {
 			// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-				
-			if(splitresult[0].equals("Abort")) {
+			
+			
+			if(splitresult[0].equalsIgnoreCase("Abort")) {
 				System.out.println("Good Bye!!!");
 				System.exit(0);
 				
-			} else if(splitresult[0].equals("Info")) {
+			} else if(splitresult[0].equalsIgnoreCase("Info")) {
 				System.out.println("Stack aqui");
 				StackSingleton.getInstance().currentState();
 				
-			} else if(splitresult[0].equals("Set")) {
-				TraceObject obj = StackSingleton.getInstance().returnlastObj();
-				Field field = obj.objecto.getClass().getDeclaredField(splitresult[1]);
-				System.out.println("Field: " + field + " tipo do Object " + field.getType());
-				field.set(obj.objecto, toObject(field.getType(), splitresult[2]));
+			} else if(splitresult[0].equalsIgnoreCase("Set")) {
+				if (splitresult.length < 3) {
+					System.out.println("You must specify a field and a value to be set.");
+				} else {
+					TraceObject obj = StackSingleton.getInstance().returnlastObj();
+					if (obj != null) {
+						Field field = null;
+						try {
+							field = obj.objecto.getClass().getDeclaredField(splitresult[1]);
+							field.setAccessible(true);
+							//System.out.println("Field: " + field + " tipo do Object " + field.getType());
+							field.set(obj.objecto, toObject(field.getType(), splitresult[2]));
+						} catch (NoSuchFieldException nsfe){ 
+							// field com este nome nao existe
+							System.out.println("The Object does not have a field \""+splitresult[1]+"\".");
+						} catch (SecurityException se) { 
+							// nao conseguimos tornar este field acessivel
+							System.out.println("Field \""+splitresult[1]+"\" could not be made accessible.");
+						} catch (IllegalArgumentException iae) { 
+							// valor de tipo errado para este field
+							System.out.println("The value provided was the wrong type ("+splitresult[2].getClass().getName()+") for field \""+splitresult[1]+"\" ("+field.getType()+").");
+						} catch (IllegalAccessException iae) { 
+							// field protegido/privado
+							System.out.println("Field \""+splitresult[1]+"\" is protected.");
+						}
+					} else
+						System.out.println("Null Object has no fields.");
+				}
 				
-			} else if(splitresult[0].equals("Get")) {
-				TraceObject obj = StackSingleton.getInstance().returnlastObj();
-				//System.out.println(obj.objecto.getClass() + "Class");
-				Field field = obj.objecto.getClass().getDeclaredField(splitresult[1]);
-				System.out.println(field.get(obj.objecto));
+			} else if(splitresult[0].equalsIgnoreCase("Get")) {
+				if (splitresult.length > 1){
+					TraceObject obj = StackSingleton.getInstance().returnlastObj();
+					if (obj!=null) {
+						try {
+							Field field = obj.objecto.getClass().getDeclaredField(splitresult[1]);
+							field.setAccessible(true);
+							System.out.println(field.get(obj.objecto));
+						} catch (NoSuchFieldException nsfe){
+							System.out.println("The Object does not have a field \""+splitresult[1]+"\".");
+						}
+					} else {
+						System.out.println("Null Object has no fields (Possible static method call without instantiation).");
+					}
+				} else {
+					System.out.println("No field specified. You must specify the field's name.");
+				}				
 				
-			} else if(splitresult[0].equals("Throw")) {
+			} else if(splitresult[0].equalsIgnoreCase("Throw")) {
 				throw ex;
 				
-			} else if(splitresult[0].equals("Retry")) {
 				
-			} else if(splitresult[0].equals("Return")) {
-				System.out.println("entrei");
-				System.out.println(method.getName() + " method name " + method.getReturnType() + " Class " + splitresult[1] + " Valor ");
+			} else if(splitresult[0].equalsIgnoreCase("Retry")) {
+								
+				
+			} else if(splitresult[0].equalsIgnoreCase("Return")) {
+				//System.out.println(method.getName() + " method name " + method.getReturnType() + " Class " + splitresult[1] + " Valor ");
 				//toObject(method.getReturnType(), splitresult[1]);
-				System.out.println(toObject(method.getReturnType(), splitresult[1]));
+				//System.out.println(toObject(method.getReturnType(), splitresult[1]));
 				return toObject(method.getReturnType(), splitresult[1]);
 				
+			} else if(splitresult[0].equalsIgnoreCase("Help") || splitresult[0].equalsIgnoreCase("?")) {
+				System.out.println("Available Commands:");
+				System.out.println("ABORT : terminates the execution of this application");
+				System.out.println("INFO : provides information on object and stack");
+				System.out.println("SET field value: sets object field with value");
+				System.out.println("GET value : gets value of object field");
+				System.out.println("THROW : re-throws exception");
+				System.out.println("RETRY : retries the last method call");
+				System.out.println("RETURN value : returns value");
+				System.out.println("HELP : shows this list.");
+				System.out.println("---");
 			} else
 				System.out.println("Invalid Command");
 		}
